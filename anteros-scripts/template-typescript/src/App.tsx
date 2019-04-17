e79a071fb8d48e739d7e075bfc359a0be40ab50a
edson <Anteros@727204>
2019-04-16 21:04:59 -0300

    Versão inicial.

---
+++ anteros-scripts/template-typescript/src/App.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './anteros_logo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
